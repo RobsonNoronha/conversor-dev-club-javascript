642acb30263bad241396815611826a72f97a0853
RobsonNoronha <robsonnoronha91@gmail.com>
2023-07-10 11:55:01 -0300

    Atualização em tempo real do servidor

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 const convertButton = document.querySelector("#convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
-function convertValues(){
+const convertValues = async () => {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real
     const currencyValueToConverted = document.querySelector(".currency-value") //valor em outras moedas
     
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+    console.log(data)
 
-    
-    const dolarToday = 5.02
-    const euroToday = 5.38
+    const dolarToday = data.USDBRL.high
+    const euroToday = data.EURBRL.high
     const libraToday = 6.27
-    const bitcoinToday = 139.985
+    const bitcoinToday = data.BTCBRL.high
 
 
     if(currencySelect.value == "dolar"){
@@ -78,7 +79,7 @@ function changeCurrency() {
         currancyImage.src = "./assets/bitcoin.png"
     }
 
-    convertValues()
+    
 }
 currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
